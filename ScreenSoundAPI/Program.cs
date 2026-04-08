18a6b2ece0b6858db0ae7fbfd984d8f880b03df3
Lucas Garcia <lucasugarcia@gmail.com>
2026-04-07 21:09:20 -0300

    Início do projeto, criação de um http client, requisição de um json e tratativa de exceção com try-catch

---
+++ ScreenSoundAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using (var client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+    }
+    catch (Exception ex) 
+    {
+        Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+    }
+}
