4aadd3deaf3bf6bd8a850efbad84356fbf4e5bae
Lucas Garcia <lucasugarcia@gmail.com>
2026-04-13 21:33:17 -0300

    Criação da classe Musica, desserializando o retorno da API em uma lista e desenvolvimento inicial dos desafios

---
+++ ScreenSoundAPI/Modelos/Musica.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSoundAPI.Modelos;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Genero: {Genero}");
+        Console.WriteLine($"Duracao em segundos: {Duracao / 1000}");
+    }
+}
--- ScreenSoundAPI/Program.cs
+++ ScreenSoundAPI/Program.cs
@@ -1,9 +1,49 @@
-﻿using (var client = new HttpClient())
+﻿using ScreenSoundAPI.Modelos;
+using System.Text.Json;
+
+using (var client = new HttpClient())
 {
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        #region Trazendo somente gêneros
+
+        var generos = musicas.SelectMany(m => m.Genero.Trim().Split(",")).SelectMany(g => g.Trim().Split("/")).Distinct();
+
+        Console.WriteLine($"Generos: - {string.Join(", ", generos)}");
+
+        #endregion
+
+        #region Ordenando artistas por nome
+
+        var artistasOrdenados = musicas.Select(m => m.Artista.Trim()).Distinct().Order();
+
+        Console.WriteLine($"Artistas:\n{string.Join("\n- ", artistasOrdenados)}");
+
+        #endregion
+
+        #region Trazendo artistas por gênero
+
+        var genero = generos.Where(g => g.Contains("rock")).First();
+
+        var artistasPorGenero = musicas.Where(m => m.Genero.Contains(genero)).Select(m => m.Artista).Distinct().Order();
+
+        Console.WriteLine($"Artistas do gênero {genero}:\n- {string.Join("\n- ", artistasPorGenero)}");
+
+        #endregion
+
+        #region Trazendo músicas de um artista
+
+        var artista = artistasPorGenero.First();
+
+        var musicasArtista = musicas.Where(m => m.Artista == artista).Select(m => m.Nome.Trim()).Distinct().Order();
+
+        Console.WriteLine($"Músicas do artista {artista}:\n- {string.Join("\n- ", musicasArtista)}");
+
+        #endregion
     }
     catch (Exception ex) 
     {
